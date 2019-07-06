72adbd0806e6ad5a7d7cbc53f3010c3b42b9f0e3
Michael Nease <michaelnease@michaelnease.com>
2019-07-05 19:35:06 -0700

    add react

---
+++ src/Button/Button.container.js
@@ -0,0 +1,8 @@
+import React, { Component } from "react";
+import Button from "./Button";
+
+export class ButtonContainer extends Component {
+  render() {
+    return <Button />;
+  }
+}
--- src/hello.js
+++
@@ -1,7 +0,0 @@
-import world from "./world";
-
-const place = world();
-
-const hello = () => `hello, ${place}`;
-
-export default hello;
