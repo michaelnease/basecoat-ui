da911d43f0cd627b1fc5bd49abae0cf7c671063f
Michael Nease <michaelnease@michaelnease.com>
2019-07-10 19:33:07 -0700

    styled components

--- packages/basecoat-core/src/Button/Button.js
+++ packages/basecoat-core/src/Button/Button.js
@@ -1,7 +1,8 @@
 import React from "react";
+import { ButtonStyled } from "./Button.styles";
 
 const Button = () => {
-  return <button>React Button!</button>;
+  return <ButtonStyled>React Button!</ButtonStyled>;
 };
 
 export default Button;
--- packages/basecoat-core/webpack.config.base.js
+++ packages/basecoat-core/webpack.config.base.js
@@ -25,10 +25,7 @@ module.exports = {
             ],
             "@babel/preset-react"
           ],
-          plugins: [
-            "@babel/plugin-proposal-class-properties",
-            "@babel/plugin-proposal-export-default-from"
-          ]
+          plugins: ["@babel/plugin-proposal-class-properties"]
         }
       }
     ]
